b3ba4894179bdafc4a15529949e6debcb48198c3
SihamEl23 <elghazi.siham1101@gmail.com>
2023-02-17 06:52:48 -0800

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * main - The entry point of C programs
+  * @void: The function takes no parameters
+  *
+  * Description: This program prints the alphabet in lowercase, followed by
+  * a new line. You are to use only the putchar function not more than two
+  * times.
+  * Return: 0
+  */
+int main(void)
+{
+	char lower_alphabet = 'z';
+
+	while (lower_alphabet >= 'a')
+	{
+		putchar(lower_alphabet);
+		lower_alphabet--;
+	}
+	putchar('\n');
+	return (0);
+}
