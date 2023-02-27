0d0a08ba6f9d10151628894e7220bb2ad8a38a9d
SihamEl23 <elghazi.siham1101@gmail.com>
2023-02-27 08:29:59 -0800

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,9 +14,10 @@ void puts_half(char *str)
 	{
 		len_s++;
 	}
-	n = len_s / 2;
 	if (len_s % 2 != 0)
 		n = (len_s - 1) / 2;
+	else
+		n = len_s / 2;
 	for (i = n; i < len_s; i++)
 		_putchar(str[i]);
 	_putchar('\n');
