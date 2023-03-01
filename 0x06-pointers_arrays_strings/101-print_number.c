fe2767eaae3707f49d2bd128e8da253715211d1b
SihamEl23 <elghazi.siham1101@gmail.com>
2023-03-01 08:39:24 -0800

    Task 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,14 +7,16 @@
  */
 void print_number(int n)
 {
+	unsigned int u;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		u = -n;
 	}
-	if (n >= 10)
+	if (u >= 10)
 	{
-		print_number(n / 10);
+		print_number(u / 10);
 	}
-	_putchar('0' + n % 10);
+	_putchar((u % 10) + '0');
 }
