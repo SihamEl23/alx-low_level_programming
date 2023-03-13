db931df619fad83ab96911312a7a2a009009b1d8
SihamEl23 <elghazi.siham1101@gmail.com>
2023-03-13 05:31:11 -0700

    Task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,10 +14,12 @@ char *_strdup(char *str)
 	int len = 0, i;
 	char *dup_str;
 
+	if (str == NULL)
+		return (NULL);
 	while (str[len] != '\0')
 		len++;
 	dup_str = malloc((len + 1) * sizeof(char));
-	if (str == NULL || dup_str == NULL)
+	if (dup_str == NULL)
 		return (NULL);
 	for (i = 0; i < len; i++)
 		dup_str[i] = str[i];
