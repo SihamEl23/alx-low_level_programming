2c7793dcdbc7b27868a575d8fabb45e277d2093a
SihamEl23 <elghazi.siham1101@gmail.com>
2023-03-30 05:35:24 -0700

    Task 0 done and update header file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
+ *
+ * Return: the number of nodes printed
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t num_nodes = 0;
+	const list_t *tmp;
+
+	tmp = h;
+	while (tmp != NULL)
+	{
+		if (tmp->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", tmp->len, tmp->str);
+		num_nodes++;
+		tmp = tmp->next;
+	}
+	return (num_nodes);
+}
