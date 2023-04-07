64c3c1a4aa2d58286d2d42fc27f45b9a624de2c0
SihamEl23 <elghazi.siham1101@gmail.com>
2023-04-07 16:47:37 -0700

    Task 4 done

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,7 +11,7 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int shift_left, invert;
+	unsigned long int shift_left, invert;
 
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
