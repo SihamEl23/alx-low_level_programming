4ce1d802347ec785e921138b70c72e490bfd43a8
SihamEl23 <elghazi.siham1101@gmail.com>
2023-04-10 14:41:13 -0700

    Task 3 done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,104 +1,93 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
-#define BUFFER_SIZE 1024
-
-void copy_file(int fd_from, int fd_to);
-void print_usage(void);
-void handle_error(const char *message, const char *file, int code);
-void set_permissions(const char *filename);
 /**
- * main - Entry point for the program
+ * error_file - checks if files can be opened.
+ * @file: file descriptor.
+ * @filename: name of file associated with file descriptor.
  *
- * @argc: Number of arguments passed to the program
- * @argv: Array of strings containing the arguments passed to the program
+ * Return: no return.
+ */
+void error_file(int file, char *filename)
+{
+	if (file == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+}
+
+/**
+ * error_write - checks if file can be written to.
+ * @file: file descriptor.
+ * @filename: name of file associated with file descriptor.
  *
- * Return: 0 if successful, otherwise an error code
+ * Return: no return.
+ */
+void error_write(int file, char *filename)
+{
+	if (file == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
+}
+
+/**
+ * error_close - checks if file descriptor can be closed.
+ * @fd: file descriptor.
+ *
+ * Return: no return.
+ */
+void error_close(int fd)
+{
+	int err_close;
+
+	err_close = close(fd);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ *
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	int fd_from;
-	int fd_to;
-	ssize_t	bytes_read;
-	char buffer[BUFFER_SIZE];
+	int file_from, file_to;
+	ssize_t nchars, nwr;
+	char buf[1024];
 
 	if (argc != 3)
 	{
-		print_usage();
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		handle_error("Can't read from file", argv[1], 98);
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-		handle_error("Can't write to file", argv[2], 99);
-	copy_file(fd_from, fd_to);
-	if (close(fd_from) == -1 || close(fd_to) == -1)
-		handle_error("Can't close file descriptor", NULL, 100);
-	set_permissions(argv[2]);
+
+	file_from = open(argv[1], O_RDONLY);
+	error_file(file_from, argv[1]);
+
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	error_write(file_to, argv[2]);
+
+	while ((nchars = read(file_from, buf, 1024)))
+	{
+		if (nchars == -1)
+			error_file(-1, argv[1]);
+
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_write(0, argv[2]);
+	}
+
+	error_close(file_from);
+	error_close(file_to);
+
 	return (0);
 }
-/**
- * copy_file - Copies data from one file descriptor to another
- *
- * @fd_from: File descriptor to read from
- * @fd_to: File descriptor to write to
- *
- * Return: None
- */
-void copy_file(int fd_from, int fd_to)
-{
-	ssize_t	bytes_read;
-	char buffer[BUFFER_SIZE];
-
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-		if (write(fd_to, buffer, bytes_read) != bytes_read)
-			handle_error("Can't write to file", NULL, 99);
-	if (bytes_read == -1)
-		handle_error("Can't read from file", NULL, 98);
-}
-/**
- * print_usage - Prints usage instructions to standard error
- *
- * Return: None
- */
-void print_usage(void)
-{
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-}
-/**
- * handle_error - Handles errors and exits the program
- *
- * @message: Error message to print
- * @file: The name of the file that caused the error
- * @code: The exit code associated with the error
- *
- * Return: None
- */
-void handle_error(const char *message, const char *file, int code)
-{
-	if (file)
-		dprintf(STDERR_FILENO, "Error: %s %s\n", message, file);
-	else
-		dprintf(STDERR_FILENO, "Error: %s\n", message);
-	exit(code);
-}
-/**
- * set_permissions - Sets permissions for a file
- *
- * @filename: Name of the file to set permissions for
- *
- * Return: None
- */
-void set_permissions(const char *filename)
-{
-	struct stat st;
-
-	if (stat(filename, &st) == -1)
-		handle_error("Can't stat file", NULL, 100);
-	if (chmod(filename, st.st_mode | S_IRUSR | S_IWUSR | S_IRGRP |
-				S_IWGRP | S_IROTH) == -1)
-		handle_error("Can't change file permissions", NULL, 100);
-}
-
