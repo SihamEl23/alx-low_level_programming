be49b66658e3eedc660e2a1bfdbe544056840f93
SihamEl23 <elghazi.siham1101@gmail.com>
2023-04-10 18:16:02 -0700

    Task 3 done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,82 +1,85 @@
 #include "main.h"
-#include <stdio.h>
 
-void check_file_open(int source_fd, int dest_fd, char *argv[]);
-void close_file(int fd, char *filename);
+#define BUFFER_SIZE 1024
 
 /**
- * main - Copies the contents of a file to another file.
- * @argc: The number of arguments.
- * @argv: The arguments vector.
+ * print_usage - print usage
  *
- * Return: Always 0.
+ * Return: void
+ */
+void print_usage(void)
+{
+	printf("Usage: cp file_from file_to\n");
+	exit(1);
+}
+
+/**
+ * print_error - print error
+ * @message: error message
+ * @filename: name of file
+ * @code: exit code
+ *
+ * Return: void
+ */
+void print_error(const char *message, const char *filename, int code)
+{
+	printf("Error: %s %s\n", message, filename);
+	exit(code);
+}
+
+/**
+ * copy_file - copy file
+ * @src_fd: source file descriptor
+ * @dst_fd: destination file descriptor
+ *
+ * Return: void
+ */
+void copy_file(int src_fd, int dst_fd)
+{
+	char buffer[BUFFER_SIZE];
+	ssize_t num_bytes_read;
+
+	while ((num_bytes_read = read(src_fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		ssize_t num_bytes_written = write(dst_fd, buffer, num_bytes_read);
+
+		if (num_bytes_written == -1)
+			print_error("Can't write to", "file", 99);
+	}
+}
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0 on success, 1 on failure
  */
 int main(int argc, char *argv[])
 {
-	int source_fd, dest_fd, chars_read, chars_written, close_result;
-	char buffer[1024];
-
 	if (argc != 3)
-	{
-		fprintf(stderr, "Usage: cp file_from file_to\n");
-		return (97);
-	}
-	source_fd = open(argv[1], O_RDONLY);
-	dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	check_file_open(source_fd, dest_fd, argv);
-	chars_read = 1024;
-	while (chars_read == 1024)
-	{
-		chars_read = read(source_fd, buffer, 1024);
-		if (chars_read == -1)
-		{
-			check_file_open(-1, 0, argv);
-		}
-		chars_written = write(dest_fd, buffer, chars_read);
-		if (chars_written == -1)
-			check_file_open(0, -1, argv);
-	}
-	close_file(source_fd, argv[1]);
-	close_file(dest_fd, argv[2]);
-	return (0);
-}
+		print_usage();
+	int src_fd = open(argv[1], O_RDONLY);
 
-/**
- * check_file_open - Checks if files can be opened.
- * @source_fd: The source file descriptor.
- * @dest_fd: The destination file descriptor.
- * @argv: The arguments vector.
- *
- * Return: No return.
- */
-void check_file_open(int source_fd, int dest_fd, char *argv[])
-{
-	if (source_fd == -1)
-	{
-		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (dest_fd == -1)
-	{
-		fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-}
+	if (src_fd == -1)
+		print_error("Can't read from", argv[1], 98);
+	int dst_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
-/**
- * close_file - Closes a file descriptor.
- * @fd: The file descriptor.
- * @filename: The name of the file.
- *
- * Return: No return.
- */
-void close_file(int fd, char *filename)
-{
-	int result = close(fd);
+	if (dst_fd == -1)
+		print_error("Can't write to", argv[2], 99);
+	copy_file(src_fd, dst_fd);
 
-	if (result == -1)
+	if (close(src_fd) == -1)
 	{
-		fprintf(stderr, "Error: Can't close file %s\n", filename);
+		printf("Error: Can't close fd %d\n", src_fd);
 		exit(100);
 	}
+
+	if (close(dst_fd) == -1)
+	{
+		printf("Error: Can't close fd %d\n", dst_fd);
+		exit(100);
+	}
+
+	return (0);
 }
