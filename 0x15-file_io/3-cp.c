22e968eeb5469e32a16a154373ddcb0302202363
SihamEl23 <elghazi.siham1101@gmail.com>
2023-04-10 19:26:49 -0700

    Task 3 done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,7 +24,8 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		return (98);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
+	| S_IRGRP | S_IWGRP | S_IROTH, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
