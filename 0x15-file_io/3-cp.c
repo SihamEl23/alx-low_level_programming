926f51d372a6b5a558791a47f1af3465646236c4
SihamEl23 <elghazi.siham1101@gmail.com>
2023-04-10 17:51:02 -0700

    Task 3 done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,72 +1,82 @@
 #include "main.h"
 #include <stdio.h>
 
+void check_file_open(int source_fd, int dest_fd, char *argv[]);
+void close_file(int fd, char *filename);
+
 /**
- * error_file - checks if files can be opened.
- * @file_from: file_from.
- * @file_to: file_to.
- * @argv: arguments vector.
- * Return: no return.
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments.
+ * @argv: The arguments vector.
+ *
+ * Return: Always 0.
  */
-void error_file(int file_from, int file_to, char *argv[])
+int main(int argc, char *argv[])
 {
-	if (file_from == -1)
+	int source_fd, dest_fd, chars_read, chars_written, close_result;
+	char buffer[1024];
+
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		fprintf(stderr, "Usage: cp file_from file_to\n");
+		return (97);
+	}
+	source_fd = open(argv[1], O_RDONLY);
+	dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	check_file_open(source_fd, dest_fd, argv);
+	chars_read = 1024;
+	while (chars_read == 1024)
+	{
+		chars_read = read(source_fd, buffer, 1024);
+		if (chars_read == -1)
+		{
+			check_file_open(-1, 0, argv);
+		}
+		chars_written = write(dest_fd, buffer, chars_read);
+		if (chars_written == -1)
+			check_file_open(0, -1, argv);
+	}
+	close_file(source_fd, argv[1]);
+	close_file(dest_fd, argv[2]);
+	return (0);
+}
+
+/**
+ * check_file_open - Checks if files can be opened.
+ * @source_fd: The source file descriptor.
+ * @dest_fd: The destination file descriptor.
+ * @argv: The arguments vector.
+ *
+ * Return: No return.
+ */
+void check_file_open(int source_fd, int dest_fd, char *argv[])
+{
+	if (source_fd == -1)
+	{
+		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (file_to == -1)
+	if (dest_fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 }
 
 /**
- * main - check the code for ALX students.
- * @argc: number of arguments.
- * @argv: arguments vector.
- * Return: Always 0.
+ * close_file - Closes a file descriptor.
+ * @fd: The file descriptor.
+ * @filename: The name of the file.
+ *
+ * Return: No return.
  */
-int main(int argc, char *argv[])
+void close_file(int fd, char *filename)
 {
-	int file_from, file_to, err_close;
-	ssize_t nchars, nwr;
-	char buf[1024];
+	int result = close(fd);
 
-	if (argc != 3)
+	if (result == -1)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-		exit(97);
-	}
-
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_from, file_to, argv);
-
-	nchars = 1024;
-	while (nchars == 1024)
-	{
-		nchars = read(file_from, buf, 1024);
-		if (nchars == -1)
-			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
-		if (nwr == -1)
-			error_file(0, -1, argv);
-	}
-
-	err_close = close(file_from);
-	if (err_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		fprintf(stderr, "Error: Can't close file %s\n", filename);
 		exit(100);
 	}
-
-	err_close = close(file_to);
-	if (err_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
-	return (0);
 }
